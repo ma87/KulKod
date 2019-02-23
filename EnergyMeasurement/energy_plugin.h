60ef865c48ec6fb29e25c4fcfc8694f18bc2ac3a
ma87 <mathieu.coic@free.fr>
2019-02-23 20:26:04 +0100

    forgot plugin header file

---
+++ EnergyMeasurement/energy_plugin.h
@@ -0,0 +1,27 @@
+#ifndef __ENERGY_PLUGIN__H__
+#define __ENERGY_PLUGIN__H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum
+{
+    RAPL,
+    TPLINK_HS110
+} TYPE_PLUGIN;
+
+typedef void * energy_data_t;
+typedef double (*get_energy_func)(energy_data_t data);
+typedef struct
+{
+    get_energy_func f;
+    energy_data_t data;
+    TYPE_PLUGIN p;
+} energy_plugin_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
